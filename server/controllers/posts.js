a6bb29af14decabf749cb14a9b28c018ac9bb668
hitnei <hoangnhi050298@gmail.com>
2021-01-15 14:36:02 +0700

    init

---
+++ server/controllers/posts.js
@@ -0,0 +1,22 @@
+import PostMessage from "../models/PostMessage.js";
+
+export const getPost = async (req, res) => {
+  try {
+    const postMessage = await PostMessage.find();
+    console.log(postMessage);
+    res.status(200).json(postMessage);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const port = req.body;
+  const newPost = new PostMessage(port);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
---
+++ server/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import postRoutes from "./routes/posts.js";
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "30bm", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30bm", extended: true }));
+app.use(cors());
+
+// connect mongodb
+const CONNECTION_URL =
+  "mongodb+srv://admin:admin@cluster0.72df8.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+  })
+  .catch((err) => console.log(err.message));
+
+//   make sure we dont get any warningin the console
+mongoose.set("useFindAndModify", false);
